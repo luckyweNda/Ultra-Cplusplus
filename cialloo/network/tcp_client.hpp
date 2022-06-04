027db6e4172c1fe7bdec49fef46ec43e3c8cac1f
luckyweNda <aluckywenda@gmail.com>
2022-06-04 20:44:23 +0800

    add receive and return string

--- cialloo/network/tcp_client.hpp
+++ cialloo/network/tcp_client.hpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <string_view>
 #include <memory>
-
+#include <algorithm>
 #include <boost/asio.hpp>
 
 namespace cialloo {
@@ -24,10 +24,14 @@ public:
 public:
     void send(std::string message);
     void send(const char* message, size_t size);
+    std::string receive();
 
 public:
     int get_port() const;
     std::string_view get_hostname();
+
+private:
+    std::string buffer_to_string(const boost::asio::mutable_buffer& buffer);
 
 private:
     int port_;
@@ -36,6 +40,22 @@ private:
     std::shared_ptr<boost::asio::ip::tcp::socket> socket_;
     std::shared_ptr<boost::asio::ip::tcp::endpoint> endpoint_;
 };
+
+inline std::string tcp_client::buffer_to_string(const boost::asio::mutable_buffer& buffer)
+{
+    char temp[buffer.size() + 1];
+    std::copy(buffer.data(), buffer.data() + buffer.size(), temp);
+    temp[buffer.size()] = '\0';
+    std::string return_value(temp);
+    return return_value;
+}
+
+inline std::string tcp_client::receive()
+{
+    boost::asio::mutable_buffer buffer;
+    socket_->receive(buffer);
+    return buffer_to_string(buffer);
+}
 
 inline void tcp_client::send(const char* message, size_t size)
 {
@@ -64,7 +84,7 @@ inline tcp_client::tcp_client(std::string hostname, int port)
     socket_ = std::make_shared<boost::asio::ip::tcp::socket>(io_context_);
     endpoint_ = std::make_shared<boost::asio::ip::tcp::endpoint>(boost::asio::ip::address::from_string(hostname_), port_);
     socket_->connect(*endpoint_);
-    socket_->non_blocking(true);
+    //socket_->non_blocking(true);
 }
 
 inline tcp_client::~tcp_client()
