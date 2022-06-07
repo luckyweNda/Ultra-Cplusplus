b1a4087a3ff02fd3222fe4b07d52d5786319b05f
luckyweNda <aluckywenda@gmail.com>
2022-06-07 19:13:07 +0800

    finish rcon

--- cialloo/network/rcon.hpp
+++ cialloo/network/rcon.hpp
@@ -12,6 +12,8 @@ constexpr int TYPE_SIZE = 4;
 constexpr int TERMINATOR_SIZE = 1;
 constexpr int HEADER_SIZE = ID_SIZE + TYPE_SIZE + TERMINATOR_SIZE;
 
+constexpr int PACKET_MAX_SIZE = 4096;
+
 } // static
 
 namespace cialloo {
@@ -53,6 +55,8 @@ private:
     void authorize();
     void send_packet(packet pack);
     unsigned char* packet_to_byte(packet pack);
+    packet pack_packet(std::string cmd, packet_type type);
+    uint32_t byte4_to_uint32(unsigned char* byte4);
 
 private:
     uint32_t id_;
@@ -62,9 +66,52 @@ private:
     std::shared_ptr<boost::asio::ip::tcp::endpoint> endpoint_;
 };
 
+inline uint32_t rcon::byte4_to_uint32(unsigned char* byte4)
+{
+    return static_cast<uint32_t>(byte4[0] | byte4[1] << 8 | byte4[2] << 16 | byte4[3] << 24);
+}
+
+inline rcon::packet rcon::pack_packet(std::string cmd, packet_type type)
+{
+    packet p;
+    p.size_ = cmd.length() + HEADER_SIZE;
+    p.id_ = id_;
+    p.type_ = type;
+    p.body_ = cmd;
+    return p;
+}
+
 inline std::string rcon::send_command(std::string cmd)
 {
+    packet p1 = pack_packet(cmd, packet_type::SERVERDATA_EXECCOMMAND);
+    unsigned char* byte_array = packet_to_byte(p1);
+    id_++;
+    packet p2 = pack_packet("\0", packet_type::SERVERDATA_RESPONSE_VALUE);
+    unsigned char* validator = packet_to_byte(p2);
+    socket_->send(boost::asio::buffer(byte_array, p1.size_ + SIZE_FIELD_SIZE));
+    socket_->send(boost::asio::buffer(validator, p2.size_ + SIZE_FIELD_SIZE));
 
+    std::string respond;
+
+    while(true)
+    {
+        unsigned char byte4[4];
+        socket_->receive(boost::asio::buffer(byte4, sizeof(byte4)));
+        auto packet_size = byte4_to_uint32(byte4);  // size
+        unsigned char recv_packet[packet_size];
+        socket_->receive(boost::asio::buffer(recv_packet, packet_size));
+        std::copy(recv_packet, recv_packet + 4, byte4);
+        auto id = byte4_to_uint32(byte4);  // id
+
+        if(id == id_) break;
+
+        char body[packet_size - ID_SIZE - TYPE_SIZE];
+        std::copy(recv_packet + ID_SIZE + TYPE_SIZE , recv_packet + packet_size, body);
+        respond += body;
+    }
+
+    id_++;
+    return respond;
 }
 
 inline unsigned char* rcon::packet_to_byte(packet pack)
@@ -87,15 +134,11 @@ inline void rcon::send_packet(packet pack)
 
 inline void rcon::authorize()
 {
-    packet p;
-    p.id_ = id_;
-    p.type_ = packet_type::SERVERDATA_AUTH;
-    p.body_ = password_;
-    p.size_ = password_.length() + HEADER_SIZE;
-    send_packet(p);
+    send_packet(pack_packet(password_, packet_type::SERVERDATA_AUTH));
     char temp[HEADER_SIZE + SIZE_FIELD_SIZE + 1 /* SERVERDATA_RESPONSE_VALUE body is a null teminator */];
     socket_->receive(boost::asio::buffer(temp, sizeof(temp)));
     socket_->receive(boost::asio::buffer(temp, sizeof(temp)));
+    id_++;
 }
 
 inline rcon::rcon(std::string hostname, int port, std::string password)
